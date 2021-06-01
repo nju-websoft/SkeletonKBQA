5a87b0df350c15950e3fac8146e0fb2f826c5c60
simba0626 <ywsun@smail.nju.edu.cn>
2021-06-01 23:14:07 +0800

    rm kbcqa code 0601

--- kbcqa/method_ir/grounding/grounding_online_ir/candidate_generation_args.py
+++
@@ -1,8 +0,0 @@
-has_bi_direction = False
-consider_literal = False
-
-kb_prefix = 'http://dbpedia.org/resource/'
-# kb_prefix = 'http://rdf.freebase.com/ns/'
-
-kb_type_predicate_list = ['a', 'rdf:type', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type']
-oracle_canidates_file = 'E:\kb_oracle_canidates_file'
